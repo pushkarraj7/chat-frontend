5d78a3ddb71e59ba72dceb8d8b260d4518de7981
puahkarraj7 <rajpushkar556@gmail.com>
2025-04-29 16:40:44 +0530

    add backend url

--- src/Chat.js
+++ src/Chat.js
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from "react";
 import io from "socket.io-client";
 import "./Chat.css";
 
-const socket = io("http://localhost:5000"); // change to backend URL
+// const socket = io("http://localhost:5000"); // change to backend URL
+const socket = io("https://chat-backend-uj3u.onrender.com");
 
 const Chat = () => {
   const [username, setUsername] = useState("");
